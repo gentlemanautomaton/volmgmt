9d5581cd8d0c110bdb6c6a40ea3563332a144073
Joshua Sjoding <ga@josh.hey.nu>
2017-10-26 20:44:43 -0700

    usn: Added Min and Max values

--- usn/usn.go
+++ usn/usn.go
@@ -2,3 +2,11 @@ package usn
 
 // USN represents a volume update sequence number.
 type USN int64
+
+const (
+	// Min is the smallest valid update sequence number.
+	Min = USN(0)
+
+	// Max is the largest valid update sequence number.
+	Max = USN(^uint64(0) >> 1)
+)
